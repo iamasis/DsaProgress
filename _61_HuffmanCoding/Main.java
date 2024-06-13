30fbc8e903c182c9fb696d4b951bd44dd54baf40
iamasistiwari <iamashishtiwari2004@gmail.com>
2024-06-13 09:00:43 +0000

    huffman coding added

---
+++ _61_HuffmanCoding/Main.java
@@ -0,0 +1,14 @@
+package DsaProgress._61_HuffmanCoding;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        String str = "abbccda";
+        HuffmanCoder hf = new HuffmanCoder(str);
+        String cs = hf.encode(str);
+        System.out.println(cs);
+        String dc = hf.decode(cs);
+        System.out.println(dc);
+
+
+    }
+}
